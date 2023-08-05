1189f73e02a6cc9f651b66eaeb872329a0009acf
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2023-08-05 15:03:42 -0300

    Atividades SENAI

---
+++ fpoo/atividades/lista-02/ex07.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+    Escreva um programa que leia 5 números inteiros em qualquer ordem
+    e exiba na tela ao final, os cinco números em ordem crescente.
+*/
+
+int main() {
+    return 0;
+}
