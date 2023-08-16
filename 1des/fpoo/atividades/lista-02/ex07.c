eb463f73810969cd08fbbbf5d2141338907f6e06
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2023-08-16 09:07:46 -0300

    Atividade FPOO, Lista-02

--- 1des/fpoo/atividades/lista-02/ex07.c
+++ 1des/fpoo/atividades/lista-02/ex07.c
@@ -6,7 +6,34 @@
 */
 
 int main() {
-	int a, b, c, d, e;
+	int numArr[5], order[5];
+	int n = sizeof(numArr) / sizeof(numArr[0]);
+	int i, iniPos, minPos, tempValue;
+
+	for (i = 0; i < n; i++) {
+		printf("Digite o %d valor: ", i+1);
+		scanf("%d", &numArr[i]);
+	}
+
+	int min = numArr[0];
+
+	for (iniPos = 0; iniPos < n; iniPos++) {
+        min = numArr[iniPos];
+        minPos = iniPos;
+		for (i = iniPos + 1; i < n; i++) {
+		    if (numArr[i] < min) {
+		        min = numArr[i];
+		        minPos = i;
+		    }
+		}
+		tempValue = numArr[iniPos];
+        numArr[iniPos] = min;
+        numArr[minPos] = tempValue;
+	}
+
+	for (i = 0; i < n; i++) {
+		printf("\nnumArr: %d ", numArr[i]);
+	}
 
     return 0;
 }
