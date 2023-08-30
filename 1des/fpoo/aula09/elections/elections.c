a893828078599a2024b30b335081d4599d9c40c8
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2023-08-30 16:54:32 -0300

    Atividades FPOO, Aula09

--- 1des/fpoo/aula09/elections/elections.c
+++ 1des/fpoo/aula09/elections/elections.c
@@ -12,79 +12,77 @@
 
 int main() {
 	setlocale(LC_ALL, "");
-	
+
 	char cityName[32];
 	printf("Digite o nome da cidade: ");
 	scanf("%s", &cityName);
-	
+
 	int totalCandidates;
 	printf("Digite a quantidade de candidatos a prefeitos: ");
 	scanf("%d", &totalCandidates);
-	
+
 	char names[totalCandidates][32];
 	int candidatesVotes[totalCandidates];
 	float result[totalCandidates], whiteResult;
 	int totalVotes, whiteVotes, nullVotes;
-	
-	int i, j;
+
+    totalVotes = 0;
+
+	int i;
 	for (i = 0; i < totalCandidates; i++) {
 		printf("\nDigite o nome do %d° candidato: ", i+1);
 		scanf("%s", &names[i]);
-	
+
 		printf("Digite o total de votos do mesmo: ");
 		scanf("%d", &candidatesVotes[i]);
-		
+
 		totalVotes += candidatesVotes[i];
 	}
-	printf("\nDigite o número total de votos em branco: ");
-	scanf("%d", &whiteVotes);
+    printf("\nDigite a quantidade de votos em branco: ");
+    scanf("%d", &whiteVotes);
+
 	totalVotes += whiteVotes;
 
 	printf("Digite a quantidade de votos nulos: ");
 	scanf("%d", &nullVotes);
-	
+
 	if (totalVotes < nullVotes) {
-		printf("\nEleição inválida.");
+		printf("\nEleição inválida.\n");
 		return 0;
 	}
-	
+
 	for (i = 0; i < totalCandidates; i++) {
 		result[i] = candidatesVotes[i] * 100 / (float)totalVotes;
 		whiteResult = whiteVotes * 100 / (float)totalVotes;
 	}
-	
+
+    int first = 0;
+	int second = 0;
+	for (i = 0; i < totalCandidates; i++) {
+        if (result[i] > result[first])
+            first = i;
+	}
+	for (i = 0; i < totalCandidates; i++) {
+        if (i != first && result[i] > result[second])
+            second = i;
+	}
+
 	printf("\nEleição válida.\n");
-	
+
 	printf("\nVots válidos: %d \n", totalVotes);
 	printf("Votos inválidos: %d \n", nullVotes);
-	
+
 	printf("\nCandidato \t Porcentagem\n");
 	for (i = 0; i < totalCandidates; i++) {
 		printf("%s \t %.2f\n", names[i], result[i]);
 	}
 	printf("Brancos \t %.2f\n", whiteResult);
-	
-	int first, second;
-	for (i = 0; i < totalCandidates; i++) {
-		for (j = 0; j < totalCandidates-1; j++) {
-			if (result[j] > result[j+1]) {
-				first = j;
-				printf("first: %d j: %d\n", first, j);
-			}
-		}
-		for (j = 0; j < totalCandidates-1; j++) {
-			if (result[j] > result[j+1] && result[j] != result[first]) {
-				second = j;
-				printf("second: %d j: %d\n", second, j);
-			}
-		}
-	}
-	
+
 	if (totalVotes > 200000 && candidatesVotes[first] < (totalVotes / 2 + 1))
 		printf("\nHaverá segundo turno entre %s e %s\n", names[first], names[second]);
 	else {
 		printf("\nNão haverá segundo turno.\n");
-		printf("O candidato %s venceu com %.2f porcento dos votos.\n", names[first], result[first]);
+		printf("O candidato %s venceu com %.2f porcento dos votos.\n", names[first], result[second]);
 	}
 
 	return 0;
