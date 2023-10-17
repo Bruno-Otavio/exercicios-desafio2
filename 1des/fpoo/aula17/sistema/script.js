ca0e970c0d2e66fb6a18ee38233fa2ce54ffc5fe
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2023-10-17 11:33:25 -0300

    Aula17 FPOO, Javascript

--- 1des/fpoo/aula17/sistema/script.js
+++ 1des/fpoo/aula17/sistema/script.js
@@ -1,15 +1,52 @@
-function inss(salario) {
-  if (salario <= 1320) return (salario * 7.5) / 100;
-  else if (salario <= 2571.29) return (salario * 9) / 100;
-  else if (salario <= 3856.94) return (salario * 12) / 100;
-  else if (salario <= 7507.49) return (salario * 14) / 100;
-  else return (salario * 14) / 100;
+class Person {
+  constructor(name, salario) {
+    this.name = name;
+    this.salario = salario;
+  }
+
+  inss() {
+    if (this.salario < 1320.01) return (this.salario * 7.5) / 100;
+    else if (this.salario < 2571.3) return (this.salario * 9) / 100;
+    else if (this.salario < 3856.95) return (this.salario * 12) / 100;
+    else if (this.salario < 7507.49) return (this.salario * 14) / 100;
+    else return 1051.05;
+  }
+
+  irrf() {
+    if (this.salario < 1903.99) return 0;
+    else if (this.salario < 2826.66) return (this.salario * 7.5) / 100 - 142.8;
+    else if (this.salario < 3751.06) return (this.salario * 15) / 100 - 354.8;
+    else if (this.salario < 4664.69)
+      return (this.salario * 22.5) / 100 - 636.13;
+    else return (this.salario * 27.5) / 100 - 869.36;
+  }
+
+  showHTML() {
+    let string = `
+      <label>Nome:</label><label>${this.name}</label>
+      <label>Salário Base:</label><label>${this.salario}</label>
+      <label>Salário Líquido:</label><label>${
+        this.salario - this.inss() - this.irrf()
+      }</label>`;
+
+    return string;
+  }
 }
 
-function irrf(salario) {
-  return 0;
-}
+const table = document.querySelector("#employees-table");
 
-console.log(inss(1000));
-console.log(inss(2000));
-console.log(inss(3000));
+// const employees = [];
+
+function addEmployees() {
+  let name = document.querySelector("#name").value;
+  let salary = Number(document.querySelector("#salary").value);
+  const employee = new Person(name, salary);
+
+  const div = document.createElement("div");
+  div.innerHTML = employee.showHTML();
+  console.log(div);
+  console.log(table);
+  table.appendChild(div);
+
+  // employees.push(employee);
+}
