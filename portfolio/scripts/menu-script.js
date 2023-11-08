2edfe22a0d0cc9079be4c7180039b96d1b74fd85
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2023-11-08 11:13:43 -0300

    Portífólio, Menu funcionalidade

--- portfolio/scripts/menu-script.js
+++ portfolio/scripts/menu-script.js
@@ -1,1 +1,30 @@
-var menu = document.getElementById("menu");
+var links = document.getElementById("nav-links");
+var ul = document.getElementById("links");
+var home = document.getElementById("homeID");
+
+var style = `
+  list-style-type: none;
+  display: flex;  
+`;
+
+var open = false;
+
+var menu = document.querySelector("#menu");
+
+menu.addEventListener("click", () => {
+  open = !open;
+
+  if (open) openMenu();
+  else closeMenu();
+});
+
+function openMenu() {
+  links.style.display = "flex";
+  ul.style.cssText = style;
+  home.style.display = "none";
+}
+
+function closeMenu() {
+  links.style.display = "none";
+  home.style.display = "block";
+}
