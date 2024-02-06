363050aedfb3d8d0c3eb556e5a1ef2d51a64a5a2
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-02-06 11:19:08 -0300

    Aula02 BCD, DER

--- 2des/pwfe/activities/busca-cep/script.js
+++ 2des/pwfe/activities/busca-cep/script.js
@@ -1,7 +1,21 @@
 const form = document.querySelector("#cep-form");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const cep = event.target.cep.value;
-    
+    const dados = await getInformation(cep);
+    updateValues(dados);
 });
+
+async function getInformation(cep) {    
+    const dados = await fetch(`https://cdn.apicep.com/file/apicep/${cep}.json`)
+        .then(res => res.json())
+    return dados;
+}
+
+function updateValues(data) {
+    document.querySelector("#state-text").innerHTML = data.state;
+    document.querySelector("#city-text").innerHTML = data.city;
+    document.querySelector("#district-text").innerHTML = data.district;
+    document.querySelector("#address-text").innerHTML = data.address;
+}
