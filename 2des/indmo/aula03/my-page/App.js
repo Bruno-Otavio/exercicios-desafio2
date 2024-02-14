fa78825be6c30a5c52ed53ca129bea229b0f9e42
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-02-13 22:23:16 -0300

    Aula03 INDMO, React-native APP

--- 2des/indmo/aula03/my-page/App.js
+++ 2des/indmo/aula03/my-page/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 const colors = {
+  white: "#D8DEE9",
   black: "#2D3250",
   gray: "#424769",
   lightgray: "#7077A1",
@@ -12,15 +13,33 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.headerContainer}>
+      <View style={styles.profileContainer}>
         <Image 
           source={{uri: "./assets/milia-rage.png"}}
           style={styles.image}
         />
-        <Text style={styles.name}><h1>Milia Rage</h1></Text>
+        <Text style={styles.title}><h1>Bruno Otávio</h1></Text>
       </View>
-      <View>
-
+      <View style={styles.profileContainer}>
+        <Text style={styles.title}><h2>Dados</h2></Text>
+        <Text style={styles.paragraph}>
+          <p>Morador da cidade de Pedreira</p>
+          <p>Capaz de diversas habilidadas</p>
+        </Text>
+      </View>
+      <View style={styles.profileContainer}>
+        <Text style={styles.title}><h2>Formação</h2></Text>
+        <View>
+          <Text style={styles.paragraph}>
+            Curso Técnico de Desenvolvimento de Sistemas - SENAI 
+          </Text>
+        </View>
+      </View>
+      <View style={styles.profileContainer}>
+        <Text style={styles.title}><h3>GitHub</h3></Text>
+        <Text>
+          <a style={{color: colors.white}} href="https://github.com/Bruno-Otavio">Bruno-Otavio</a>
+        </Text>
       </View>
     </View>
   );
@@ -33,10 +52,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  headerContainer: {
+  profileContainer: {
     backgroundColor: colors.black,
     borderRadius: 25,
-    padding: 50,
+    marginTop: 15,
+    marginBottom: 15,
+    padding: 40,
   },
   image: {
     width: 200,
@@ -46,9 +67,13 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: colors.beige,
   },
-  name: {
+  title: {
     color: colors.beige,
     marginRight: "auto",
     marginLeft: "auto",
+  },
+  paragraph: {
+    color: colors.white,
+    fontSize: 15,
   }
 });
