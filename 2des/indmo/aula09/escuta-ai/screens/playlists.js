ba1f99d7c0d5b79d44821b5460b457f0fc79fa02
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-04-19 18:02:35 -0300

    Aula09 INDMO, Music Player

--- 2des/indmo/aula09/escuta-ai/screens/playlists.js
+++ 2des/indmo/aula09/escuta-ai/screens/playlists.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const PlayLists = () => {
   return (
     <View style={styles.container}>
-      <View></View>
+      <View style={styles.card}>
+        <Image
+            source={{ uri: "../assets/covers/guilty-gear-cover.jpg" }}
+            style={styles.image}
+        />
+        <View>
+          <Text style={styles.title}>Guilty Gear</Text>
+          <Text style={styles.author}>Bruno</Text>
+        </View>
+      </View>
+
+
+      <View style={styles.card}>
+        <Image
+            source={{ uri: "../assets/covers/icdd.jpg" }}
+            style={styles.image}
+        />
+        <View>
+          <Text style={styles.title}>Nessa eu infarto</Text>
+          <Text style={styles.author}>Bruno</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <Image
+            source={{ uri: "../assets/covers/pericles.jpg" }}
+            style={styles.image}
+        />
+        <View>
+          <Text style={styles.title}>Churras</Text>
+          <Text style={styles.author}>Bruno</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,7 +48,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
+  image: {
+    width: 200,
+    height: 200,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 500,
+    color: "#102C57",
+  },
+  author: {
+    fontSize: 20,
+    fontWeight: 350,
+    color: "#595959"
+  },
+  card: {
+    marginBottom: 15
+  }
 });
 
 export default PlayLists;
